2838dcf4d39e597585401fcc9dee910cf51930e3
ladylilian <f22542727@gmail.com>
2021-08-28 19:02:41 +0800

    admin base - sidebar

--- app/core/urls.py
+++ app/core/urls.py
@@ -5,11 +5,12 @@ Django Urls
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from app.index.views import IndexView
+from app.index.views import AdminPanel, IndexView
 from app.admin_settings.urls import settings_url
 
 urlpatterns = [
     path('admin/settings/', include(settings_url)),
     path('django-admin/', admin.site.urls),
     path('', IndexView, name='index'),
+    path('admin',  AdminPanel, name='admin'),
 ]
