fc1d03d7e84d896474fab8c331d5c559837e25f6
jaydenteo <jayden228@gmail.com>
2021-08-31 15:17:43 +0800

    improved naming convention

--- frontend/components/Home/CarparkCard.tsx
+++ frontend/components/Home/CarparkCard.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from './styles.module.css';
-import Map from '@/app/resources/static/images/map-pin.svg'
-import NewWindow from '@/app/resources/static/images/open-in-new.svg'
+import MapIcon from '@/app/resources/static/images/map-pin.svg'
+import NewWindowIcon from '@/app/resources/static/images/open-in-new.svg'
 
 const CarparkCard = (props: any) => {
     return (
@@ -14,11 +14,11 @@ const CarparkCard = (props: any) => {
                     {props.carpark.description}
                 </div>
                 <div className={styles.directions}> 
-                    <Map />
+                    <MapIcon />
 
                     <a href={props.carpark.mapURL} target="_blank">
                         &nbsp; Get directions
-                        <NewWindow />
+                        <NewWindowIcon />
                     </a>
                 </div>
             </div>
