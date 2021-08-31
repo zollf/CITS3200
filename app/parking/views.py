0fa9b553b363bd3e2f06fc1e55f023de2bdcc5b4
Joel-Milligan <joelmilligan24@gmail.com>
2021-08-31 22:40:08 +0800

    Created POST endpoint for creating carparks

--- app/parking/views.py
+++ app/parking/views.py
@@ -1,12 +1,25 @@
+from django.http.response import JsonResponse
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 from .models import CarPark
 from .serializers import *
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def carparks_list(request):
-    data = CarPark.objects.all()
-    serializer = CarParkSerializer(data, context={'request': request}, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        data = CarPark.objects.all()
+        serializer = CarParkSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        carpark_data = JSONParser().parse(request)
+        serializer = CarParkSerializer(data=carpark_data)
+
+        if not serializer.is_valid():
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save() 
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED) 
