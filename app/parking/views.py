c1070a07b440ab6b267dfc0ec38948fa93fbd03d
Joel-Milligan <joelmilligan24@gmail.com>
2021-09-03 19:35:28 +0800

    Autopep'ed my code

--- app/parking/views.py
+++ app/parking/views.py
@@ -15,36 +15,36 @@ def carparks_list(request):
         data = CarPark.objects.all()
         serializer = CarParkSerializer(data, context={'request': request}, many=True)
         return Response(serializer.data)
-    
+
     elif request.method == 'POST':
         carpark_data = JSONParser().parse(request)
         serializer = CarParkSerializer(data=carpark_data)
 
         if not serializer.is_valid():
             return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        serializer.save() 
-        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED) 
+
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def carpark_detail(request, pk):
-    try: 
-        carpark = CarPark.objects.get(pk=pk) 
-    except CarPark.DoesNotExist: 
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND) 
- 
+    try:
+        carpark = CarPark.objects.get(pk=pk)
+    except CarPark.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
-        serializer = CarParkSerializer(carpark) 
-        return JsonResponse(serializer.data) 
- 
-    elif request.method == 'PUT': 
-        carpark_data = JSONParser().parse(request) 
-        serializer = CarParkSerializer(carpark, data=carpark_data) 
-        if serializer.is_valid(): 
+        serializer = CarParkSerializer(carpark)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        carpark_data = JSONParser().parse(request)
+        serializer = CarParkSerializer(carpark, data=carpark_data)
+        if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data) 
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
- 
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         carpark.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
