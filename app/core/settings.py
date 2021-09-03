56594a7679fe8492cd5db7a2433f7ed579323685
Joel-Milligan <joelmilligan24@gmail.com>
2021-09-03 19:29:30 +0800

    Fixed up app config

--- app/core/settings.py
+++ app/core/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django_components',
     'rest_framework',
     'app.index',
-    'app.parking.apps.ParkingConfig'
+    'app.parking',
     'app.authentication',
     'app.admin',
 ]
