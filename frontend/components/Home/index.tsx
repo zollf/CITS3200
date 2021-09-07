a58bbd9be0dc16a446c77e7e52d5a7314747eaaf
jaydenteo <jayden228@gmail.com>
2021-09-07 17:57:50 +0800

    removed card from 'home' page

--- frontend/components/Home/index.tsx
+++ frontend/components/Home/index.tsx
@@ -2,21 +2,11 @@ import React from 'react';
 // import Tick from '@/app/resources/static/images/done.svg';
 // import styles from './styles.module.css';
 
-import CarparkCard from '../CarparkCard/CarparkCard';
 
 const Home = () => {
   return (
     <div>
-      Hello from React! jayden here check test
-
-      <CarparkCard
-        carpark = {{
-          name:"Admin Carpark North",
-          description:"Carpark located somewhere." ,
-          mapURL:"https://goo.gl/maps/RytgNDB4MW8McDBY6" ,
-        }}  
-      />
-
+      Hello from React!
     </div>
   );
 };
