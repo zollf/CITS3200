655654ba05260499f3c19b7a2249df303fdec64f
jaydenteo <jayden228@gmail.com>
2021-09-07 17:58:13 +0800

    switched to main.css specific fonts

--- frontend/components/CarparkCard/CarparkCard.tsx
+++ frontend/components/CarparkCard/CarparkCard.tsx
@@ -7,7 +7,7 @@ const CarparkCard = (props: any) => {
     return (
         <div className={styles.card}>
             <div className={styles.topHalf}>
-                {props.carpark.name}
+                <h3>{props.carpark.name}</h3>
             </div>
             <div className={styles.bottomHalf}>
                 <div className={styles.description}>
@@ -15,9 +15,9 @@ const CarparkCard = (props: any) => {
                 </div>
                 <div className={styles.directions}> 
                     <MapIcon />
-                    <a href={props.carpark.mapURL} target="_blank">
+                    <a href={props.carpark.mapURL} className={"p-small-bold"} target="_blank">
                         &nbsp; Get directions
-                        <NewWindowIcon />
+                        <NewWindowIcon className={styles.newWindowIcon}/>
                     </a>
                 </div>
             </div>
