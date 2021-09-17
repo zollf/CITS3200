7c832e7984a8eb4cea89ed62fa9b11bf683fd076
APM246 <apm0366@gmail.com>
2021-09-17 22:48:50 +0800

    removed delete button for logged in user and users table highlights currently logged in user

--- app/admin/templatetags/admin.py
+++ app/admin/templatetags/admin.py
@@ -3,12 +3,12 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('table.html')
-def create_table(title: str, data: dict, headers: str):
+def create_table(title: str, data: dict, headers: str, **kwargs):
     """
     Creates table given object and headers
     Usage: `{% create_table  'carparks' carpark 'name|description' %}`
     """
-    return {'title': title, 'headers': headers.split("|"), 'data': data}
+    return {'title': title, 'headers': headers.split("|"), 'data': data, **kwargs}
 
 @register.inclusion_tag('form.html')
 def create_form(title: str, data: dict, fields: str):
