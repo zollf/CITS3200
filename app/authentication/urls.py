f41f084183ab2b49c4320a459f22f22f700af228
APM246 <apm0366@gmail.com>
2021-10-07 20:47:46 +0800

    fixed lint due to conflict between pycodestyle and autopep8

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -6,7 +6,7 @@ authentication_urls = [
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', LogoutView, name="logout"),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="reset/reset.html",
-                                                                 email_template_name="reset/email.html", subject_template_name="reset/subject.txt", success_url="/login"),
+         email_template_name="reset/email.html", subject_template_name="reset/subject.txt", success_url="/login"),
          name="reset"),
     path('password_reset_confirm/<str:uidb64>/<str:token>', auth_views.PasswordResetConfirmView.as_view(
         template_name="reset/reset_confirm.html", success_url="/login"), name="reset_confirm")
