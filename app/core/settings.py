fa53a655c627fb7d68772901c98f3da8d2af4071
APM246 <apm0366@gmail.com>
2021-10-07 19:59:53 +0800

    added white left-border to Bookings tab in admin panel

--- app/core/settings.py
+++ app/core/settings.py
@@ -93,10 +93,10 @@ AUTH_USER_MODEL = 'authentication.User'
 LOGIN_URL = "login/"
 LOGIN_REDIRECT_URL = "/"  # in case next parameter is not specified in GET request
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_HOST = 
+# EMAIL_HOST =
 #EMAIL_PORT = 25
-#EMAIL_HOST_USER = 
-#EMAIL_HOST_PASSWORD = 
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD =
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
