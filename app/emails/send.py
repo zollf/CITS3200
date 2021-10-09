a3d8ec85bfff7d0a59602c8ee7cfa2b0b898bb35
= <22709469@student.uwa.edu.au>
2021-10-09 15:54:22 +0800

    Remove try and except

--- app/emails/send.py
+++ app/emails/send.py
@@ -7,25 +7,22 @@ from datetime import date
 def log_and_send_mail(subject: str, to_email: list, category: str, template: str, data={}):
     if category not in Emails.events:
         raise KeyError(f"{category} is not in Emails.events. Please add it in before sending emails.")
-    try:
-        html_body = render_to_string(template, data)
-        msg = EmailMultiAlternatives(
-            subject=subject,
-            from_email=None,
-            to=to_email,
-        )
-        msg.attach_alternative(html_body, "text/html")
-        msg.send(fail_silently=False)
-        
-        # If msg.send does not fail then we log
-        email = Emails(
-            name=Emails.events[category]['name'],
-            description=Emails.events[category]['description'],
-            category=category,
-            payload="",
-            date=date.today(),
-        )
-        email.save()
-        return True
-    except:
-        return False
+    html_body = render_to_string(template, data)
+    msg = EmailMultiAlternatives(
+        subject=subject,
+        from_email=None,
+        to=to_email,
+    )
+    msg.attach_alternative(html_body, "text/html")
+    msg.send(fail_silently=False)
+    
+    # If msg.send does not fail then we log
+    email = Emails(
+        name=Emails.events[category]['name'],
+        description=Emails.events[category]['description'],
+        category=category,
+        payload="",
+        date=date.today(),
+    )
+    email.save()
+    return True
