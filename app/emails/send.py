2456665ca88846b2bb01b2a506450f752b213441
= <22709469@student.uwa.edu.au>
2021-10-09 15:51:01 +0800

    Update when emails are logged

--- app/emails/send.py
+++ app/emails/send.py
@@ -8,6 +8,16 @@ def log_and_send_mail(subject: str, to_email: list, category: str, template: str
     if category not in Emails.events:
         raise KeyError(f"{category} is not in Emails.events. Please add it in before sending emails.")
     try:
+        html_body = render_to_string(template, data)
+        msg = EmailMultiAlternatives(
+            subject=subject,
+            from_email=None,
+            to=to_email,
+        )
+        msg.attach_alternative(html_body, "text/html")
+        msg.send(fail_silently=False)
+        
+        # If msg.send does not fail then we log
         email = Emails(
             name=Emails.events[category]['name'],
             description=Emails.events[category]['description'],
@@ -16,14 +26,6 @@ def log_and_send_mail(subject: str, to_email: list, category: str, template: str
             date=date.today(),
         )
         email.save()
-        html_body = render_to_string(template, data)
-        msg = EmailMultiAlternatives(
-            subject=subject,
-            from_email=None,
-            to=to_email,
-        )
-        msg.attach_alternative(html_body, "text/html")
-        msg.send()
         return True
     except:
         return False
