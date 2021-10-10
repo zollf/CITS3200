b2d4f8046feb3d33b4e8948c9a8d346962eb2c23
Dylan Kennedy <dylan@theleagueagency.com.au>
2021-10-11 01:42:48 +0800

    Added Additional Bay Modal

--- frontend/components/BookingForm/steps/BaySelection/index.tsx
+++ frontend/components/BookingForm/steps/BaySelection/index.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useContext, useState } from 'react';
+import ReactModal from 'react-modal';
 import { useEffectOnce } from 'react-use';
 import * as Yup from 'yup';
 import Arrow from '@/app/resources/static/images/arrow.svg';
 import DatePicker from '@/frontend/components/DatePicker';
+import InfoIcon from '@/app/resources/static/images/info.svg';
 import Reset from '@/app/resources/static/images/reset.svg';
 import getCookie from '@/frontend/lib/GetCookie';
 import { BookingContext } from '@/frontend/components/BookingForm/index';
@@ -17,6 +19,7 @@ const BaySelection: StepComponent = () => {
   const { values, setFieldValue } = useFormikContext<BookingFormValues>();
   const { globalStartTime, globalEndTime, setLoading, setError } = useContext<BookingContext>(BookingContext);
   const [props, setProps] = useState<BaysInitialProps>();
+  const [modalDesc, setModalDesc] = useState<string | undefined>(undefined);
 
   const getBays = useCallback(async () => {
     return await fetch(`/api/carparks/${values.carpark!.pk}/bays`).then((r) => r.json());
@@ -117,83 +120,105 @@ const BaySelection: StepComponent = () => {
   }
 
   return (
-    <div className={styles.baysSelection}>
-      <h2>
-        Select bay for <span className={styles.blue}>{values.carpark!.name}</span> on {values.date.toDateString()}
-      </h2>
+    <>
+      <div className={styles.baysSelection}>
+        <h2>
+          Select bay for <span className={styles.blue}>{values.carpark!.name}</span> on {values.date.toDateString()}
+        </h2>
 
-      <div className={styles.legends}>
-        <div className={styles.legend}>
-          <div className={styles.greenSquare} />
-          <p>Selected</p>
-        </div>
-
-        <div className={styles.legend}>
-          <div className={styles.redSquare} />
-          <p>Unavailable</p>
-        </div>
-
-        <div className={styles.legend}>
-          <div className={styles.yellowSquare} />
-          <p>Available</p>
-        </div>
-      </div>
-
-      <div className={styles.bayButtons}>
-        <div className={styles.bayButtonsLeft}>
-          <DatePicker selected={values.date} onChange={handleDateChange} />
-        </div>
-        <div className={styles.bayButtonsRight}>
-          <CustomButton onClick={handleReset} type={ButtonType.button} icon={<Reset />}>
-            Reset
-          </CustomButton>
-          <CustomButton type={ButtonType.submit} icon={<Arrow />} disabled={!values.booking.size}>
-            Continue
-          </CustomButton>
-        </div>
-      </div>
-
-      <div className={styles.table} data-testid="table" onMouseLeave={() => setMouseDown(null)}>
-        <div className={styles.top}>
-          <div className={styles.topLeft}>
-            <p data-small-bold>BAY</p>
+        <div className={styles.legends}>
+          <div className={styles.legend}>
+            <div className={styles.greenSquare} />
+            <p>Selected</p>
           </div>
-          <div className={styles.times}>
-            {props.times.map((t) => (
-              <p key={`times-${t}`} data-small-bold>
-                {t}
-              </p>
+
+          <div className={styles.legend}>
+            <div className={styles.redSquare} />
+            <p>Unavailable</p>
+          </div>
+
+          <div className={styles.legend}>
+            <div className={styles.yellowSquare} />
+            <p>Available</p>
+          </div>
+        </div>
+
+        <div className={styles.bayButtons}>
+          <div className={styles.bayButtonsLeft}>
+            <DatePicker selected={values.date} onChange={handleDateChange} />
+          </div>
+          <div className={styles.bayButtonsRight}>
+            <CustomButton onClick={handleReset} type={ButtonType.button} icon={<Reset />}>
+              Reset
+            </CustomButton>
+            <CustomButton type={ButtonType.submit} icon={<Arrow />} disabled={!values.booking.size}>
+              Continue
+            </CustomButton>
+          </div>
+        </div>
+
+        <div className={styles.table} data-testid="table" onMouseLeave={() => setMouseDown(null)}>
+          <div className={styles.top}>
+            <div className={styles.topLeft}>
+              <p data-small-bold>BAY</p>
+            </div>
+            <div className={styles.times}>
+              {props.times.map((t) => (
+                <p key={`times-${t}`} data-small-bold>
+                  {t}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className={styles.bays}>
+            {props.bays.map((bay: Bay, i) => (
+              <div key={`bays-${bay.bayNum}`} className={styles.bay} data-light={i % 2 !== 0}>
+                <div className={styles.left}>
+                  <p>{bay.bayNum}</p>
+                  {bay.desc && (
+                    <button type="button" className={styles.additionInfo} onClick={() => setModalDesc(bay.desc)}>
+                      <InfoIcon />
+                    </button>
+                  )}
+                </div>
+                <div className={styles.bayTimes}>
+                  {bay.times.map((t: Time) => (
+                    <button
+                      key={t.slug}
+                      onClick={() => handleClick(t)}
+                      className={styles.bayTime}
+                      data-unavailable={t.status === selection.UNAVAILABLE}
+                      data-selected={values.booking.has(t.slug)}
+                      data-testid="bay-time"
+                      onMouseDown={() => onMouseDown(t)}
+                      onMouseUp={() => setMouseDown(null)}
+                      onMouseOver={() => handleHover(t)}
+                      onClickCapture={() => handleClick(t)}
+                      type="button"
+                    />
+                  ))}
+                </div>
+              </div>
             ))}
           </div>
         </div>
-        <div className={styles.bays}>
-          {props.bays.map((bay: Bay, i) => (
-            <div key={`bays-${bay.bayNum}`} className={styles.bay} data-light={i % 2 !== 0}>
-              <div className={styles.left}>
-                <p>{bay.bayNum}</p>
-              </div>
-              <div className={styles.bayTimes}>
-                {bay.times.map((t: Time) => (
-                  <button
-                    key={t.slug}
-                    onClick={() => handleClick(t)}
-                    className={styles.bayTime}
-                    data-unavailable={t.status === selection.UNAVAILABLE}
-                    data-selected={values.booking.has(t.slug)}
-                    data-testid="bay-time"
-                    onMouseDown={() => onMouseDown(t)}
-                    onMouseUp={() => setMouseDown(null)}
-                    onMouseOver={() => handleHover(t)}
-                    onClickCapture={() => handleClick(t)}
-                    type="button"
-                  />
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
-    </div>
+      <ReactModal
+        isOpen={!!modalDesc}
+        shouldCloseOnEsc={true}
+        onRequestClose={() => setModalDesc(undefined)}
+        shouldCloseOnOverlayClick={true}
+        className={styles.modal}
+        ariaHideApp={false}
+      >
+        {modalDesc}
+        <div className={styles.closeButton}>
+          <CustomButton onClick={() => setModalDesc(undefined)} type={ButtonType.button} icon={undefined}>
+            Close
+          </CustomButton>
+        </div>
+      </ReactModal>
+    </>
   );
 };
 
